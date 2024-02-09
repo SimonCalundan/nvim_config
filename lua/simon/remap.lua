b29f8fba3d9127f4a764de69eef735e101674849
Simon Calundan <simonkcalundan@gmail.com>
2024-02-09 13:15:25 +0100

    Added new NERDTree command

--- lua/simon/remap.lua
+++ lua/simon/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.NERDTreeToggle)
+vim.keymap.set("n", "<leader>pv", vim.cmd.NERDTreeToggle) -- Open NERDTree
+vim.keymap.set("n", "<leader>nf", vim.cmd.NERDTreeFind)  -- Open NERDTree with current file selected
 -- Når man er i visual mode, kan man trykke stort K eller J for at flytte markerede tekst
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
