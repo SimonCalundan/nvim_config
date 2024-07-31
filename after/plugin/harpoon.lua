062a8480528754b093bf7dbf9e3e1647b4fb85f3
SimonCalundan <112775651+SimonCalundan@users.noreply.github.com>
2024-07-31 09:16:26 +0200

    new keybinds for harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,9 +5,9 @@ vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 -- To remove a mark, toggle the menu and press dd, like when deleting a line
-vim.keymap.set("n", "<C-1>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-2>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-3>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-4>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
 
 
